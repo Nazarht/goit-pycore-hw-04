b16b5a5e7f0d9a3aef4b14da9dc5f1326099dec7
Nazar Hotko <nazar.hotko@outlook.com>
2025-10-25 13:40:17 +0200

    make small changes

--- assistant.py
+++ assistant.py
@@ -1,52 +1,46 @@
 import readline
-
-
-def parse_input(input_string: str) -> tuple[str, list[str]]:
-    command, *args = input_string.split()
-    return command, args
-
-
-def handle_hello(_: dict, __: list[str]) -> None:
-    print("How can I help you?")
-
-
-def handle_add(user_data: dict, args: list[str]) -> None:
-    if len(args) < 2:
-        print("Please provide name and phone number")
-        return
-    contact_name = args[0]
-    contact_phone_number = args[1]
-    user_data["contacts"][contact_name] = contact_phone_number
-    print(f"Contact {contact_name} added successfully")
-
-
-def handle_change(user_data: dict, args: list[str]) -> None:
-    if len(args) < 2:
-        print("Please provide name and new phone number")
-        return
-    contact_name = args[0]
-    contact_phone_number = args[1]
-    user_data["contacts"][contact_name] = contact_phone_number
-    print(f"Contact {contact_name} updated successfully")
-
-
-def handle_phone(user_data: dict, args: list[str]) -> None:
-    if len(args) < 1:
-        print("Please provide a contact name")
-        return
-    contact_name = args[0]
-    print(
-        f"Contact {contact_name} phone number is {user_data['contacts'].get(contact_name, 'not found')}"
-    )
-
-
-def handle_all(user_data: dict, _: list[str]) -> None:
-    for contact_name, contact_phone_number in user_data["contacts"].items():
-        print(f"{contact_name} - {contact_phone_number}")
-
+from collections import defaultdict
 
 def main():
-    user_data = {"contacts": {}}
+    contacts: defaultdict[str, str] = defaultdict(lambda: 'not found')
+    
+    def parse_input(input_string: str) -> tuple[str, list[str]]:
+        command, *args = input_string.split()
+        return command, args
+
+    def handle_hello(_: list[str]) -> None:
+        print("How can I help you?")
+
+    def handle_add(args: list[str]) -> None:
+        if len(args) < 2:
+            print("Please provide name and phone number")
+            return
+        contact_name = args[0]
+        contact_phone_number = args[1]
+        contacts[contact_name] = contact_phone_number
+        print(f"Contact {contact_name} added successfully")
+
+    def handle_change(args: list[str]) -> None:
+        if len(args) < 2:
+            print("Please provide name and new phone number")
+            return
+        contact_name = args[0]
+        contact_phone_number = args[1]
+        contacts[contact_name] = contact_phone_number
+        print(f"Contact {contact_name} updated successfully")
+
+    def handle_phone(args: list[str]) -> None:
+        if len(args) < 1:
+            print("Please provide a contact name")
+            return
+        contact_name = args[0]
+        print(
+            f"Contact {contact_name} phone number is {contacts.get(contact_name)}"
+        )
+
+    def handle_all(_: list[str]) -> None:
+        for contact_name, contact_phone_number in contacts.items():
+            print(f"{contact_name} - {contact_phone_number}")
 
     handlers: dict[str, callable] = {
         "hello": handle_hello,
@@ -78,7 +72,7 @@ def main():
             print("Invalid command")
             continue
 
-        handler(user_data, args)
+        handler(args)
 
 if __name__ == "__main__":
     main()
