889df069bb98c7842dfaf8f5425c737e67cd525a
Nazar Hotko <nazar.hotko@outlook.com>
2025-10-29 16:48:17 +0100

    last change

--- assistant.py
+++ assistant.py
@@ -1,56 +1,58 @@
 import readline
 from collections import defaultdict
 
+
+def parse_input(input_string: str) -> tuple[str, list[str]]:
+    command, *args = input_string.split()
+    return command, args
+
+def handle_hello(_: list[str], __: defaultdict[str, str]) -> None:
+    return "How can I help you?"
+
+def handle_add(args: list[str], contacts: defaultdict[str, str]) -> str:
+    if len(args) < 2:
+        return "Please provide name and phone number"
+    contact_name = args[0]
+    contact_phone_number = args[1]
+    contacts[contact_name] = contact_phone_number
+    return f"Contact {contact_name} added successfully"
+
+def handle_change(args: list[str], contacts: defaultdict[str, str]) -> str:
+    if len(args) < 2:
+        return "Please provide name and new phone number"
+    contact_name = args[0]
+    contact_phone_number = args[1]
+    contacts[contact_name] = contact_phone_number
+    return f"Contact {contact_name} updated successfully"
+
+def handle_phone(args: list[str], contacts: defaultdict[str, str]) -> str:
+    if len(args) < 1:
+        return "Please provide a contact name"
+    contact_name = args[0]
+    return f"Contact {contact_name} phone number is {contacts.get(contact_name)}"
+
+def handle_all(_: list[str], contacts: defaultdict[str, str]) -> str:
+    result: list[str] = []
+    
+    for contact_name, contact_phone_number in contacts.items():
+        result.append(f"{contact_name} - {contact_phone_number}")
+    
+    if not result:
+        return "No contacts found"
+    
+    return "\n".join(result)
+
+
+handlers: dict[str, callable] = {
+    "hello": handle_hello,
+    "add": handle_add,
+    "change": handle_change,
+    "phone": handle_phone,
+    "all": handle_all,
+}
+
 def main():
     contacts: defaultdict[str, str] = defaultdict(lambda: 'not found')
-    
-    def parse_input(input_string: str) -> tuple[str, list[str]]:
-        command, *args = input_string.split()
-        return command, args
-
-    def handle_hello(_: list[str]) -> None:
-        return "How can I help you?"
-
-    def handle_add(args: list[str]) -> str:
-        if len(args) < 2:
-            return "Please provide name and phone number"
-        contact_name = args[0]
-        contact_phone_number = args[1]
-        contacts[contact_name] = contact_phone_number
-        return f"Contact {contact_name} added successfully"
-
-    def handle_change(args: list[str]) -> str:
-        if len(args) < 2:
-            return "Please provide name and new phone number"
-        contact_name = args[0]
-        contact_phone_number = args[1]
-        contacts[contact_name] = contact_phone_number
-        return f"Contact {contact_name} updated successfully"
-
-    def handle_phone(args: list[str]) -> str:
-        if len(args) < 1:
-            return "Please provide a contact name"
-        contact_name = args[0]
-        return f"Contact {contact_name} phone number is {contacts.get(contact_name)}"
-
-    def handle_all(_: list[str]) -> str:
-        result: list[str] = []
-        
-        for contact_name, contact_phone_number in contacts.items():
-            result.append(f"{contact_name} - {contact_phone_number}")
-        
-        if not result:
-            return "No contacts found"
-        
-        return "\n".join(result)
-
-    handlers: dict[str, callable] = {
-        "hello": handle_hello,
-        "add": handle_add,
-        "change": handle_change,
-        "phone": handle_phone,
-        "all": handle_all,
-    }
 
     while True:
         try:
@@ -69,12 +71,12 @@ def main():
             print("Good bye!")
             break
 
-        handler = handlers.get(command)
+        handler = handlers.get(command, None)
         if handler is None:
             print("Invalid command")
             continue
 
-        result = handler(args)
+        result = handler(args, contacts)
         print(result)
 
 if __name__ == "__main__":
